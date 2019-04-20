c0034c49238874079ac2a9cc7fa5e00cdf458e90
GeorgyZhulimov <mr.zhulimov@mail.ru>
2019-04-20 15:34:45 +0300

    sec

--- flask_app.py
+++ flask_app.py
@@ -12,14 +12,14 @@ def hello_world():
 
 @app.route("/", methods = ["POST"])
 def main():
-    alice_request = AliceRequest
-    alice_response = AliceResponse
-    logging.info("Request {}".format(alice_request))
+    alice_request = AliceRequest(request.json)
+    alice_response = AliceResponse(alice_request) 
+    #logging.info("Request {}".format(alice_request))
 
     alice_response, session_storage[alice_request.user_id] = handle_dialog(
     alice_request, alice_response, session_storage.get(alice_request.user_id)
     )
-    logging.info("Response {}".format(alice_response))
+    #logging.info("Response {}".format(alice_response))
     return alice_response.dumps()
 
 if __name__=='__main__':
