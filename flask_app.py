bd0f4bf9142d54161c563275b669521c521577ba
GeorgyZhulimov <mr.zhulimov@mail.ru>
2019-04-20 15:45:28 +0300

    Update (again)

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 import logging
-from alisa_sdk import *
+from alice_sdk import *
 from main_function import *
 
 app = Flask(__name__)
@@ -13,7 +13,7 @@ def hello_world():
 @app.route("/", methods = ["POST"])
 def main():
     alice_request = AliceRequest(request.json)
-    alice_response = AliceResponse(alice_request) 
+    alice_response = AliceResponse(alice_request)
     #logging.info("Request {}".format(alice_request))
 
     alice_response, session_storage[alice_request.user_id] = handle_dialog(
